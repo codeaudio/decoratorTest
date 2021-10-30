8e1f75933c1fa98631cb574ec7fcf99f2bb89aa6
codeaudio <64453492+codeaudio@users.noreply.github.com>
2021-10-30 06:30:28 +0300

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -22,7 +22,7 @@ def time_result(func):
 @filter_first_key('ivan')
 def custom_filter(dictionary: dict, name_in='', year='99', key=''):
     return {
-        k: dict(filter(lambda k: str(k[0]).lower() == key.lower() if key != "" else k, v.items()))
+        k: dict(filter(lambda k: str(k[0]).lower() == key.lower() if key != '' else k, v.items()))
         for k, v in dictionary.items()
         if v.get("year") < int(year)
         if str(name_in).lower() in str(k).lower()
